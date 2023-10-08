0f682a19e6e27d6a0ec8769d19c62718b40f60d6
FelixOgundha <fogundha@strathmore.edu>
2023-10-08 23:39:16 +0300

    feature:added binary search

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class BinSearch
+{
+    static void Main()
+    {
+        int[] numArray = new int[] { 11, 22, 33, 44, 55, 66, 77 };
+
+        SearchArray(numArray, 44);
+        SearchArray(numArray, 99);
+
+        string[] elements = { "apple", "banana", "car", "dog" };
+        SearchArray(elements, "car");
+        SearchArray(elements, "elephant");
+
+    }
+
+    public static void SearchArray(Array array, object value)
+    {
+        int numIndex = Array.BinarySearch(array, value);
+        if (numIndex > 0)
+        {
+            Console.WriteLine("Item {0} is at index : {1}", value, numIndex);
+        }
+        else
+        {
+            Console.WriteLine("Item {0} is not present in the array", value);
+        }
+
+    }
+}
