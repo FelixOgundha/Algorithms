0294f1a802d6ffa3446b819e91018cc29ffa0790
FelixOgundha <fogundha@strathmore.edu>
2023-10-08 18:55:02 +0300

    updated the function

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -28,3 +28,18 @@ static int LinearSearch(int[] a, int x)
     }
     return -1;
 }
+
+
+static void Main(string[] args)
+{
+    int[] arr = new int[] { 1, 4, 5, 7, 9, 22 };
+
+    Console.WriteLine("Current rray Elements " + string.Join(" , ", arr));
+    Console.WriteLine("Enter the element to be searched");
+    int value = Convert.ToInt32(Console.ReadLine());
+    var index = LinearSearch(arr, value);
+    if (index != -1)
+        Console.WriteLine($"Element {value} found at index {index}");
+    else
+        Console.WriteLine("Element not found");
+}
